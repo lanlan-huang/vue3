f4117b25f1aad575e1f095d01031e4187835d33a
一一鸭 <2994142360@qq.com>
2023-07-16 10:18:30 +0800

    vue3的变化_组合式api和setup和ref

--- 01-搭建工程/vue3-app-vite/src/App.vue
+++ 01-搭建工程/vue3-app-vite/src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <p>
-    <button @click="handleIncrease">count: {{ count }}</button>
+    <button @click="countRef++">count: {{ countRef }}</button>
+    <button @click="handleIncrease">count: {{ countRef }}</button>
   </p>
 </template>
 
 <script>
+// ref用于创建一个响应式数据
+import { ref } from "vue";
+
+
 export default {
-  data() {
+  setup() {
+    // 1. console.log('在所有的声明周期钩子函数之前调用')
+    //2.  console.log(this); setup函数里面的this为undefined
+    let countRef = ref(0); //会返回一个响应式的数字
+
+    // 3. 在setup中，count时一个对象
+    //    实际代理中count是一个count.value
+    const handleIncrease = () => {
+      countRef.value++;
+    }
+    // 新增
+
+    // 修改
+
+    // 删除
+
+    // 4. 需要将使用到的东西return给模板和其它的组件
     return {
-      count: 0,
+      countRef,
+      handleIncrease,
     }
-  },
-  methods:{
-    handleIncrease(){
-      console.log(this);
-      this.count++;
-    }
+
   }
 }
 </script>
---
+++ 01-搭建工程/vue3-app-vite/src/App1.vue
@@ -0,0 +1,39 @@
+<template>
+    <h1>count:{{ countRef }}</h1>
+    <p>
+        <button @click="handleDecrease">count: {{ countRef }}</button>
+        <button @click="handleIncrease">count: {{ countRef }}</button>
+    </p>
+</template>
+  
+<script>
+// ref用于创建一个响应式数据
+import { ref } from "vue";
+// 5. 使用组合式api代码不会零散、杂乱
+function useCount() {
+    let countRef = ref(0);
+    const handleIncrease = () => {
+        countRef.value++;
+    };
+    const handleDecrease = () => {
+        countRef.value--;
+    }
+    return {
+        countRef,
+        handleIncrease,
+        handleDecrease
+    }
+}
+
+export default {
+    setup() {
+        return {
+            ...useCount(), // 解构赋值
+        }
+
+    }
+}
+</script>
+  
+<style></style>
+
