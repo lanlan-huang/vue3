63f32b25749739846801370b5beeb2fbef49d399
一一鸭 <2994142360@qq.com>
2023-07-23 20:08:49 +0800

    模板中的变化v-for、v-if和key和Fragment

--- 模板中的变化/src/App.vue
+++ 模板中的变化/src/App.vue
@@ -1,11 +1,30 @@
 <template>
+  <h2>vue3可以写多个根节点</h2>
   <div id="app">
-    <CheckEditor v-model="checked" v-model:text.trim="title" />
+    <div class="container">
+      <div class="list">
+        <strong>编辑：</strong>
+        <div class="list">
+          <CheckEditor v-for="item in producets" v-model="item.sell" v-model:text.trim="item.title" />
+        </div>
+      </div>
+      <div class="list">
+        <strong>销售中：</strong>
+        <!-- 当使用`<template>`进行`v-for`循环时，需要把`key`值放到`<template>`中，而不是它的子元素中 -->
+        <template v-for="(item, index) in sellingRef" :key="item.id">
+          <div>
+            <span>{{ index }}.</span>
+            <strong>{{ item.title }}</strong>
+          </div>
+        </template>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import CheckEditor from './components/CheckEditor.vue';
+import { ref,computed } from "vue"
 const defaultSells = [
   {
     id: '1',
@@ -20,13 +39,32 @@ export default {
   components: {
     CheckEditor
   },
-  data() {
+  setup() {
+    const producetsRef = ref(defaultSells);
+    const sellingRef = computed(() => producetsRef.value.filter((it) => it.sell))
     return {
-      checked: true,
-      title: 'xiaomi',
+      producets: producetsRef,
+      sellingRef,
     }
+
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  margin-top: 50px;
+  width: 880px;
+  margin: 50px auto;
+}
+
+.list {
+  display: flex;
+  margin: 1em 0;
+  align-items: center;
+}
+
+strong {
+  margin-right: 1em;
+}
+</style>
